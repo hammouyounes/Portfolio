73f7daed117e5415601205046f650115b7d2148f
youness <younesshamou013@gmail.com>
2026-02-11 17:35:16 +0100

    feat: add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import HomePage from './pages/HomePage';
 import LinksPage from './pages/LinksPage';
 import AchievementsPage from './pages/AchievementsPage';
 import ScrollProgress from './components/ui/ScrollProgress';
+import { Analytics } from "@vercel/analytics/react"
 
 const App: React.FC = () => {
   return (
     <Router>
+      <Analytics />
       <ScrollProgress />
       <Routes>
         <Route path="/" element={<HomePage />} />
